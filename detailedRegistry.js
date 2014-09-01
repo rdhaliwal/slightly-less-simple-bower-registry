f22bea7e9ea44d6f1842b6b33bd1125c79c80637
rdhaliwal <dhaliwal.randeep.s@gmail.com>
2014-09-01 22:33:25 +1000

    Listing the registry when navigating to /view using hbs template

--- detailedRegistry.js
+++ detailedRegistry.js
@@ -1,9 +1,27 @@
 'use strict';
 
+var fs = require('fs'),
+    handlebars = require('handlebars'),
+    registry = require('./initRegistry');
 
 
 var getRegistryList = function (request, result) {
-    console.log('Hello world');
+    var templateFile = fs.readFileSync('viewRegistryList.hbs', 'utf-8');
+    var hbTemplate = handlebars.compile(templateFile);
+
+    var temp = [];
+    for (var name in registry.packages) {
+        temp.push({
+            name: name,
+            url: registry.packages[name]
+        });
+    }
+    var mustacheData = {
+        bowerPackages : temp
+    };
+
+    result.send(hbTemplate(mustacheData));
+
 };
 
 exports.getRegistryList = getRegistryList;
