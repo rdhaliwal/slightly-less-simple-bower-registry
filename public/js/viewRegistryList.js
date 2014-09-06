0d5589c0b881893b3ff50d9f8753e0fe8066626b
rdhaliwal <dhaliwal.randeep.s@gmail.com>
2014-09-06 18:30:59 +1000

    - Refactored the bower info method to be a common method called, instead of duplicated.  - Cleaned up template a bit  - Made the package URL appear in the description loaded section

--- public/js/viewRegistryList.js
+++ public/js/viewRegistryList.js
@@ -2,54 +2,41 @@
 
 $(document).ready(function() {
 
+    function loadPackageInfo(element) {
+        var tempUrl = "/view/" + $(element).data("package-name");
+        var $package = $(element);
+        var $version = $(element).find('.js-bowerPackageVersion');
+        var $detailsContainer = $(element).find('.BowerPackage-details');
+        var $details = $(element).find('.js-bowerPackageDetails');
+
+        if ($package.attr('data-loaded-data') !== 'true') {
+            $detailsContainer.addClass('is-loading');
+            console.log('Firing off request to:' + tempUrl);
+            $.ajax({
+                url: tempUrl,
+                cache: true
+            })
+            .done(function( result ) {
+                $detailsContainer.removeClass('is-loading');
+                $details.append(result);
+                $details.show();
+                $version.html($details.find('.BowerPackage-latestVersionNumber').html());
+                $package.attr('data-loaded-data', 'true');
+            });
+        }
+    }
+
     function handleBowerPackages() {
         console.log('Running handleBowerPackages');
         $('.js-bowerPackage[open]').each(function(){
-
-            var tempUrl = "/view/" + $(this).data("package-name");
-            var $package = $(this);
-            var $version = $(this).find('.js-bowerPackageVersion');
-            var $details = $(this).find('.BowerPackage-details');
-
-            if ($package.attr('data-loaded-data') !== 'true') {
-                $details.addClass('is-loading');
-                console.log('Firing off request to:' + tempUrl);
-                $.ajax({
-                    url: tempUrl,
-                    cache: true
-                })
-                .done(function( result ) {
-                    $details.removeClass('is-loading');
-                    $details.append(result);
-                    $version.html($details.find('.BowerPackage-latestVersionNumber').html());
-                    $package.attr('data-loaded-data', 'true');
-                });
-            }
+            loadPackageInfo(this);
         });
     }
 
     function initPage() {
         handleBowerPackages();
         $('.js-bowerPackage').on('click', function(){
-            var tempUrl = "/view/" + $(this).data("package-name");
-            var $package = $(this);
-            var $version = $(this).find('.js-bowerPackageVersion');
-            var $details = $(this).find('.BowerPackage-details');
-
-            if ($package.attr('data-loaded-data') !== 'true') {
-                $details.addClass('is-loading');
-                console.log('Firing off request to:' + tempUrl);
-                $.ajax({
-                    url: tempUrl,
-                    cache: true
-                })
-                .done(function( result ) {
-                    $details.removeClass('is-loading');
-                    $details.append(result);
-                    $version.html($details.find('.BowerPackage-latestVersionNumber').html());
-                    $package.attr('data-loaded-data', 'true');
-                });
-            }
+            loadPackageInfo(this);
         });
     }
 
