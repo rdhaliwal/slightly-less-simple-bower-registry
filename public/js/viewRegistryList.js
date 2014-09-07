06e4ee1dd153b9ee8ef834581a25d6220bac5aa9
rdhaliwal <dhaliwal.randeep.s@gmail.com>
2014-09-07 18:54:53 +1000

    Adding search functionality

--- public/js/viewRegistryList.js
+++ public/js/viewRegistryList.js
@@ -38,6 +38,19 @@ $(document).ready(function() {
         $('.js-bowerPackage').on('click', function(){
             loadPackageInfo(this);
         });
+
+        //TODO: Clean this up
+        var $rows = $('.js-bowerPackage');
+        $('#searchField').keyup(function() {
+            var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+
+            $rows.show().filter(function() {
+                var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+                return !~text.indexOf(val);
+            }).hide();
+        });
+
+
     }
 
     initPage();
